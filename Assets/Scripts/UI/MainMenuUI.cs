202fab9515ae43e7415bab6f94a4359462658561
Andrey Trushkeen <a.trushkin@fast-report.com>
2019-11-13 19:40:14 +0300

    MainMenu added

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public GameObject MainElements;
+    public GameObject SettingsElements;
+
+    public void StartDebugLevel()
+    {
+        SceneManager.LoadScene("DebugScene", LoadSceneMode.Single);
+    }
+
+    public void OpenSettings()
+    {
+        //MainElements.SetActive(false);
+        //SettingsElements.SetActive(true);
+    }
+
+    public void LeaveGame()
+    {
+        Application.Quit();
+    }
+}
