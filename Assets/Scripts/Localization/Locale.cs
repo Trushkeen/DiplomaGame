5e423ced2ce2081364f8dfb82d1fafaecd424feb
Andrey Trushkeen <a.trushkin@fast-report.com>
2019-11-14 16:47:42 +0300

    added localiztion system

---
+++ Assets/Scripts/Localization/Locale.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public static class Locale
+{
+    public static string Language { get; private set; } = "ru";
+    private static Dictionary<string, string> CurrentLocale;
+
+    public static void LoadLocale()
+    {
+        CurrentLocale = new Dictionary<string, string>();
+        var loc = Resources.Load<TextAsset>("Translations\\" + Language).text.Split('-');
+        foreach (var i in loc)
+        {
+            var str = i.Split('=');
+            if (str[0].Length > 2)
+                CurrentLocale.Add(str[0].Replace("=", ""), str[1].Replace("-", ""));
+        }
+    }
+
+    public static void ChangeLanguage(string lng)
+    {
+        if (lng.Length > 2 || lng.Length < 2)
+        {
+            throw new System.Exception("Wrong format");
+        }
+        else
+        {
+            Language = lng;
+        }
+    }
+
+    public static string Get(string key)
+    {
+        return CurrentLocale[key.ToLower()];
+    }
+}
---
+++ Assets/Scripts/Localization/LocalizedText.cs
@@ -0,0 +1,13 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizedText : MonoBehaviour
+{
+    public string DictionaryKey;
+
+    private void Start()
+    {
+        GetComponent<TextMeshProUGUI>().text = Locale.Get(DictionaryKey);
+    }
+}
