d83d3790f6d0e1f7b4117e931449c8618c151a59
MaratAlaev <46092593+MaratAlaev@users.noreply.github.com>
2019-11-17 20:54:54 +0300

    SpawnMob

---
+++ Assets/Scripts/spawn/SpawnMob.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpawnMob : MonoBehaviour
+{
+    GameObject obj;
+    Vector3 SpawnVector;
+    public int maxSpawn = 5;
+    private bool AbleToSpawn = true;
+    private void Start()
+    {
+        obj = GameObject.FindGameObjectWithTag("Enemy");
+        SpawnVector = new Vector3(transform.position.x, 26F, transform.position.z);
+    }
+
+    private void FixedUpdate()
+    {
+        if (AbleToSpawn && GameObject.FindGameObjectsWithTag("Enemy").Length <= maxSpawn)
+        {
+            var obje = Instantiate(obj, SpawnVector,transform.rotation).transform.parent = null;
+            obje.GetComponent<NavMeshAgent>().Warp(gameObject.transform.position);
+
+            StartCoroutine(SpawnCooldown());
+        }
+
+    }
+
+    private IEnumerator SpawnCooldown()
+    {
+        AbleToSpawn = false;
+        yield return new WaitForSeconds(15.0F);
+        AbleToSpawn = true;
+
+    }
+}
