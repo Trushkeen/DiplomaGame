399c292c9b05536d416f2d3e02f4d8e654439b12
Andrey Trushkeen <a.trushkin@fast-report.com>
2019-11-23 21:40:59 +0300

    tank looks at player

--- Assets/Scripts/Enemies/MilitaryTankController.cs
+++ Assets/Scripts/Enemies/MilitaryTankController.cs
@@ -5,21 +5,31 @@ using UnityEngine.AI;
 
 public class MilitaryTankController : MonoBehaviour
 {
+    public GameObject TankShell;
+    public GameObject TankShellPoint;
+
+    public GameObject Tower;
+
+    private NavMeshAgent NavAgent;
+    private GameObject Player;
+
     void Start()
     {
-        
+        NavAgent = GetComponent<NavMeshAgent>();
+        Player = FindObjectOfType<PlayerMovement>().gameObject;
     }
 
     void Update()
     {
         MoveToTarget();
+        Vector3 targetPos = new Vector3(Player.transform.position.x, transform.position.y, Player.transform.position.z);
+        Tower.transform.LookAt(targetPos);
     }
 
     public void MoveToTarget()
     {
-        var agent = GetComponent<NavMeshAgent>();
-        var playerPos = FindObjectOfType<PlayerMovement>().transform.position;
-        agent.speed = GetComponent<Mob>().Speed;
-        agent.SetDestination(playerPos);
+        var playerPos = Player.transform.position;
+        NavAgent.speed = GetComponent<Mob>().Speed;
+        NavAgent.SetDestination(playerPos);
     }
 }
