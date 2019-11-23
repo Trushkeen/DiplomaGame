ee4c6b9fd3ad50bd19e8d47d142ccc3bdb6581c9
Andrey Trushkeen <a.trushkin@fast-report.com>
2019-11-23 13:00:54 +0300

    colliders upd

--- Assets/Scripts/Enemies/MilitaryTankController.cs
+++ Assets/Scripts/Enemies/MilitaryTankController.cs
@@ -5,13 +5,11 @@ using UnityEngine.AI;
 
 public class MilitaryTankController : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         MoveToTarget();
