2ecc92b83564ce96799412dad43bdf06236f3d4c
Andrey Trushkeen <a.trushkin@fast-report.com>
2019-12-07 12:40:50 +0300

    WIP: players manager

---
+++ Assets/Scripts/PlayersManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersManager : MonoBehaviour
+{
+    private static PlayersManager Instance = null;
+
+    private PlayersManager() { }
+
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if(Instance == this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+
+    }
+}
