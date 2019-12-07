cafe27d971deb47c33ccb77e464f8518cece604d
Andrey Trushkeen <a.trushkin@fast-report.com>
2019-12-07 15:41:50 +0300

    singleton and id getter

--- Assets/Scripts/PlayersManager.cs
+++ Assets/Scripts/PlayersManager.cs
@@ -6,6 +6,8 @@ public class PlayersManager : MonoBehaviour
 {
     private static PlayersManager Instance = null;
 
+    public List<PlayerStats> Players;
+
     private PlayersManager() { }
 
     void Start()
@@ -22,8 +24,18 @@ public class PlayersManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    void Update()
-    {
+    public static PlayersManager GetInstance() => Instance;
 
+    public int GetAvailableID()
+    {
+        int id = Random.Range(0, 1000);
+        foreach (var i in Players)
+        {
+            if(i.ID == id)
+            {
+                GetAvailableID();
+            }
+        }
+        return id;
     }
 }
