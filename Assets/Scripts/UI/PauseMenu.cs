5ae4108ea46593fe8f0915a44795bd9aa6b122fd
GlebStryukov <g.striukov@fast-report.com>
2020-03-26 18:36:04 +0300

    Quit menu and pause
    asd

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+    public GameObject Player;
+    // Update is called once per frame
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+                Cursor.visible = false;
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        GameIsPaused = false;
+        MouseMove.Unfreeze();
+        Player.GetComponentInChildren<AutomaticRifle>().enabled = true;
+        Cursor.visible = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        GameIsPaused = true;
+        MouseMove.Freeze();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Player.GetComponentInChildren<AutomaticRifle>().enabled = false;
+    }
+
+    public void LoadMenu()
+    {
+        MouseMove.Unfreeze();
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+    }
+}
