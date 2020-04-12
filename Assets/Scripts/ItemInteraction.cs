bd761311f212a25d44b74332fcfb079df9a4a2f3
Andrey Trushkeen <a.trushkin@fast-report.com>
2020-04-12 12:39:39 +0300

    small refactoring

--- Assets/Scripts/ItemInteraction.cs
+++ Assets/Scripts/ItemInteraction.cs
@@ -19,20 +19,25 @@ public class ItemInteraction : MonoBehaviour
     {
         if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, 100F, ~(12 << 8), QueryTriggerInteraction.Ignore))
         {
-            InteractionGO.SetActive(true);
-            if (!Locale.LocaleLoaded) Locale.LoadLocale();
-            InteractionText.text = Locale.Get("presstopickup") + hit.collider.gameObject.name;
-            if (Input.GetKeyUp(KeyCode.E))
+            var go = hit.collider.gameObject;
+            if (go.tag == "Loot")
             {
-                var go = hit.collider.gameObject;
-                var loot = go.GetComponent<LootableItem>().Loot;
-                if (Inventory.Instance.Items.Count < Inventory.Instance.Space)
+                InteractionGO.SetActive(true);
+
+                if (!Locale.LocaleLoaded) Locale.LoadLocale();
+
+                InteractionText.text = Locale.Get("presstopickup") + hit.collider.gameObject.name;
+                if (Input.GetKeyUp(KeyCode.E))
                 {
-                    var cell = Instantiate(Inventory.Instance.BasicCell, InventoryUI.transform);
-                    var slot = cell.GetComponent<InventorySlot>();
-                    slot.AddItem(loot);
-                    Inventory.Instance.Items.Add(cell);
-                    Destroy(go);
+                    var loot = go.GetComponent<LootableItem>().Loot;
+                    if (Inventory.Instance.Items.Count < Inventory.Instance.Space)
+                    {
+                        var cell = Instantiate(Inventory.Instance.BasicCell, InventoryUI.transform);
+                        var slot = cell.GetComponent<InventorySlot>();
+                        slot.AddItem(loot);
+                        Inventory.Instance.Items.Add(cell);
+                        Destroy(go);
+                    }
                 }
             }
         }
