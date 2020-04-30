e407e0d2f6c5e4140c492443aa78f7b068e235f2
Andrey Trushkeen <a.trushkin@fast-report.com>
2020-04-30 22:45:44 +0300

    finished

--- Assets/Scripts/Weapons/Weapons Behaviour/SniperRifle.cs
+++ Assets/Scripts/Weapons/Weapons Behaviour/SniperRifle.cs
@@ -2,17 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SniperRifle : MonoBehaviour
+public class SniperRifle : MonoBehaviour, IWeaponBase
 {
-    // Start is called before the first frame update
-    void Start()
+    public WeaponBase WB;
+
+    public GameObject BulletPoint;
+
+    public LineRenderer LineRend;
+
+    private bool AbleToShoot = true;
+    private WeaponSoundController Emitter;
+    private Animator AnimController;
+
+    private void Start()
     {
-        
+        WB = GetComponent<WeaponBase>();
+        Emitter = GetComponent<WeaponSoundController>();
+        AnimController = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
+    public void Reload()
+    {
+        AnimController.SetBool("Reloading", false);
+        if (WB.AmmoTotal >= WB.AmmoClip)
+        {
+            WB.AmmoTotal += WB.AmmoNow;
+            WB.AmmoNow = WB.AmmoClip;
+            WB.AmmoTotal -= WB.AmmoClip;
+        }
+        else if (WB.AmmoTotal - (WB.AmmoClip - WB.AmmoNow) < 0)
+        {
+            WB.AmmoNow += WB.AmmoTotal;
+            WB.AmmoTotal = 0;
+        }
+        else if (WB.AmmoTotal < WB.AmmoClip)
+        {
+            var diff = WB.AmmoClip - WB.AmmoNow;
+            WB.AmmoNow += diff;
+            WB.AmmoTotal -= diff;
+        }
+    }
+
+    public void Shoot()
+    {
+        if (AbleToShoot && WB.AmmoNow > 0)
+        {
+            Emitter.PlayShot();
+            if (Physics.Raycast(BulletPoint.transform.position, -BulletPoint.transform.forward, out RaycastHit hit, 100F, ~(2 << 8), QueryTriggerInteraction.Ignore))
+            {
+                Debug.DrawRay(BulletPoint.transform.position, BulletPoint.transform.forward * -100F, Color.red, 10f);
+                
+                var go = hit.collider.gameObject;
+                while (go.transform.parent != null)
+                {
+                    go = go.transform.parent.gameObject;
+                }
+                if (go.CompareTag("Enemy"))
+                {
+                    go.GetComponent<Mob>().DealDamage(WB.Damage);
+                }
+            }
+            MouseMove.ShakeCamera();
+            WB.AmmoNow--;
+            StartCoroutine(ShootingCooldown());
+        }
+    }
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(Controls.FireBtn) && AnimController.GetBool("Reloading") == false)
+        {
+            Shoot();
+        }
+        if (Input.GetKeyDown(Controls.ReloadBtn) && AnimController.GetBool("Reloading") == false && WB.AmmoNow != WB.AmmoClip)
+        {
+            //Reload();
+            AnimController.SetBool("Reloading", true);
+        }
+    }
+
+    private IEnumerator ShootingCooldown()
+    {
+        AbleToShoot = false;
+        yield return new WaitForSeconds(1F);
+        AbleToShoot = true;
+    }
+
+    private void PrintAmmo()
+    {
+        print(WB.AmmoNow + "/" + WB.AmmoTotal);
     }
 }
