b0c3ebd584dd9fc884f5a82479b9cd2d96d067dd
marat.alaev <m.alaev@fast-report.com>
2020-05-07 18:38:36 +0300

    map

---
+++ Assets/LightBlink.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class LightBlink : MonoBehaviour
+{
+    Light Light;
+    void Start()
+    {
+        Light = GetComponent<Light>();
+    }
+
+    void FixedUpdate()
+    {
+        var ran = Random.value;
+        if (ran <= .2)
+        {
+            Light.enabled = true;
+        }
+        else 
+        {
+            Light.enabled = false;
+        }
+
+    }
+}
