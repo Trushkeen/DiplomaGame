57be30ad0c6a6c3e556e86366e2e4093daa077ef
Andrey Trushkeen <a.trushkin@fast-report.com>
2020-05-27 22:24:27 +0300

    death screen fix

---
+++ Assets/Scripts/Utils/DeathScreenControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathScreenControl : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
