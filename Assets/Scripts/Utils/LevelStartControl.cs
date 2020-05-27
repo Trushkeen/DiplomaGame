a5cada5c5563f6dafacbdc3c028354e33b2a417c
Andrey Trushkeen <a.trushkin@fast-report.com>
2020-05-27 22:30:11 +0300

    level load cursor fix

---
+++ Assets/Scripts/Utils/LevelStartControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelStartControl : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
