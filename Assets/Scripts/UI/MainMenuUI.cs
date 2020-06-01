25f8a311cf503488658774eaf860953e874d106b
GlebStryukov <g.striukov@fast-report.com>
2020-06-01 22:08:08 +0300

    + added manager for UI
    + added manager for UI
    * changed colors for slider

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -5,18 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
-    public GameObject MainElements;
-    public GameObject SettingsElements;
-
     public void StartDebugLevel()
     {
         SceneManager.LoadScene("City_Level1", LoadSceneMode.Single);
     }
 
-    public void OpenSettings()
+    public void ShowTab(GameObject obj)
     {
-        //MainElements.SetActive(false);
-        //SettingsElements.SetActive(true);
+        obj.SetActive(true);
+    }
+
+    public void HideTab(GameObject obj)
+    {
+        obj.SetActive(false);
     }
 
     public void LeaveGame()
