bf3899a7f513bd85173577eb545d6542d0b86a18
Andrey Trushkeen <a.trushkin@fast-report.com>
2020-06-15 22:11:14 +0300

    language switcher

---
+++ Assets/Scripts/UI/MainMenuLocaleSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuLocaleSelector : MonoBehaviour
+{
+    public GameObject SelectLocaleGroup;
+
+    public void RevertGroupVisibility()
+    {
+        if (SelectLocaleGroup.activeSelf)
+        {
+            SelectLocaleGroup.SetActive(false);
+        }
+        else
+        {
+            SelectLocaleGroup.SetActive(true);
+        }
+    }
+
+    public void SelectLocale(string locale)
+    {
+        Locale.ChangeLanguage(locale);
+        Locale.LoadLocale();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
