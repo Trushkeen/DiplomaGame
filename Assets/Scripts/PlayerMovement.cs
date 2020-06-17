db75f8fd6ec7469e6545ed3241ebf217eb220270
Andrey Trushkeen <a.trushkin@fast-report.com>
2020-06-17 19:33:43 +0300

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -37,7 +37,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(Controls.JumpBtn))
         {
             Debug.DrawRay(transform.position, Vector3.down * 15F, Color.red, 5F);
-            if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 15F, ~(-1 << 8), QueryTriggerInteraction.Ignore))
+            if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 15F,, QueryTriggerInteraction.Ignore))
             {
                 Rigid.AddForce(transform.up * 300F, ForceMode.Acceleration);
             }
